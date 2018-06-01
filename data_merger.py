ee7ac1429c498dfa18b3e4f7abfea2a1b9dcd05a
wcl1 <wcl1@meitu.com>
2018-06-01 17:57:11 +0800

    update

---
+++ data_merger.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+# -- coding: utf-8 --
+"""
+Copyright (c) 2018. All rights reserved.
+Created by C. L. Wang on 2018/5/3
+"""
+import collections
+import os
+import sys
+import time
+import numpy as np
+from datetime import datetime
+
+p = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if p not in sys.path:
+    sys.path.append(p)
+
+from multiprocessing import Pool
+from root_dir import ROOT_DIR
+from data_preprocessor import check_error_features
+from utils import time_2_readable, traverse_dir_files
+
+
+def get_path_clz(path):
+    """
+    获取音频类的名称，即原音频和增强音频
+    """
+    path_name = path.split('/')[-1].split('-')[0]
+    return path_name.split('_')[0]
+
+
+def get_path_name(path):
+    """
+    获取音频的名称
+    """
+    path_name = path.split('/')[-1].split('-')[0]
+    return path_name.split('_')[1]
+
+
+def get_label_dict(path_list):
+    """
+    根据音频路径列表，生成标签字典
+    """
+    normal_num = 4  # 音频的正常样本数量，小于即认为异常样本
+
+    label_dict = collections.defaultdict(int)
+
+    for path in path_list:
+        clz_name = get_path_clz(path)
+        label_dict[clz_name] += 1
+
+    for key in label_dict.keys():
+        if label_dict[key] < normal_num:
+            print '[INFO] 异常类别: %s, 样本数: %s' % (key, label_dict[key])
+            label_dict.pop(key, None)  # 删除类别
+
+    return label_dict
+
+
+def prc_func(path, label):
+    """
+    子进程，加载数据
+    """
+    try:
+        print '[INFO] ' + path
+        features = np.load(path)
+        name = get_path_name(path)
+        if np.min(features) == np.max(features):
+            raise Exception('features error')
+        return features, label, name
+    except Exception as e:
+        print "[INFO] 错误: %s" % str(e)
+        return np.array([]), -1, None
+
+
+def mp_data_merger(data_dir, out_dir, n_prc=40):
+    """
+    合并数据，数据清洗
+    :param data_dir: 文件路径
+    :param n_prc: 进程数
+    :return: None
+    """
+    start_time = datetime.now()  # 起始时间
+    print "[INFO] 当前时间: %s" % time_2_readable(time.time())
+
+    path_list, _ = traverse_dir_files(data_dir)  # 路径列表
+    label_dict = get_label_dict(path_list)
+    print "[INFO] 音频总数: %s" % len(path_list)
+
+    results = []
+    p = Pool(processes=n_prc)  # 进程数尽量与核数匹配
+    for path in path_list:
+        clz_name = get_path_clz(path)
+        if clz_name not in label_dict:
+            print "[INFO] %s 类别样本过少! " % clz_name
+            continue
+        results.append(p.apply_async(prc_func, args=(path, clz_name,)))
+
+    count = 0
+    f_list, l_list, n_list = [], [], []
+    print len(results)
+    for i in results:
+        features, label, name = i.get()
+        if not name or label == -1 or check_error_features(features):  # 检查异常数据
+            print "[INFO] 错误数据"
+            continue
+        f_list.append(features)
+        l_list.append(label)
+        n_list.append(name)
+        count += 1
+        if count % 1000 == 0:
+            print "\t[INFO] count %s" % count
+    print "\t[INFO] count %s" % count
+
+    print(l_list)
+    print(n_list)
+    np.savez(out_dir, f_list=f_list, l_list=l_list, n_list=n_list)
+    res_data = np.load(out_dir)
+    print "[INFO] 最终数据: %s %s, %s %s, %s %s" % (
+        'f_list', res_data['f_list'].shape, 'l_list', res_data['l_list'].shape, 'n_list', res_data['n_list'].shape)
+    print "[INFO] 最终特征: %s, %s, %s" % (res_data['f_list'][0], res_data['l_list'][0], res_data['n_list'][0])
+    print "[INFO] 结束时间: %s" % time_2_readable(time.time())
+    elapsed_time = datetime.now() - start_time  # 终止时间
+    print "[INFO] 耗时: %s (秒)" % elapsed_time
+
+
+def merge_data():
+    """
+    合并数据，npz格式，f_list是特征矩阵，l_list是标签列表，n_list是名称列表
+    """
+    npy_path = os.path.join(ROOT_DIR, 'data', 'npy_data')
+    npz_out = os.path.join(ROOT_DIR, "data", "data_all.npz")
+    # mp_data_merger(data_dir=test_path, out_dir=test_out)
+    mp_data_merger(npy_path, npz_out)
+
+
+if __name__ == '__main__':
+    merge_data()
---
+++ hash/hash_preprocessor_mxnet.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# -- coding: utf-8 --
+"""
+Copyright (c) 2018. All rights reserved.
+Created by C. L. Wang on 2018/5/8
+"""
+import os
+import sys
+
+import mxnet as mx
+import numpy as np
+from mxnet import gluon
+
+from root_dir import ROOT_DIR
+
+p = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if p not in sys.path:
+    sys.path.append(p)
+
+
+class HashPreProcessor(object):
+    def __init__(self):
+        self.model = None
+        self.data_dir = os.path.join(ROOT_DIR, 'data')
+        self.gpu = True
+        pass
+
+    def process(self):
+        print('[INFO] 转换开始')
+        if self.gpu:
+            ctx = mx.gpu(0)  # GPU模型
+        else:
+            ctx = mx.cpu(0)  # CPU模型
+
+        sym = os.path.join(self.data_dir, "triplet_net.json")
+        params = os.path.join(self.data_dir, "triplet_loss_model_88_0.9934.params")
+        self.model = gluon.nn.SymbolBlock(outputs=mx.sym.load(sym), inputs=mx.sym.var('data'))
+        self.model.load_params(params, ctx=ctx)
+
+        file_name = 'data_train_v2.npz'
+        data_path = os.path.join(ROOT_DIR, 'experiments', file_name)
+        data_all = np.load(data_path)
+        X_test1 = data_all['f_list']
+        l_list1 = data_all['l_list']
+        n_list1 = data_all['n_list']
+
+        print('[INFO] X_test1.shape: ' + str(X_test1.shape))
+        print('[INFO] l_list1.shape: ' + str(l_list1.shape))
+        print('[INFO] n_list1.shape: ' + str(n_list1.shape))
+
+        file_name = 'data_test_v2.npz'
+        data_path = os.path.join(ROOT_DIR, 'experiments', file_name)
+        data_all = np.load(data_path)
+        X_test2 = data_all['f_list']
+        l_list2 = data_all['l_list']
+        n_list2 = data_all['n_list']
+        print('[INFO] X_test2.shape: ' + str(X_test2.shape))
+
+        X_test = np.concatenate((X_test1, X_test2), axis=0)
+        l_list = np.concatenate((l_list1, l_list2), axis=0)
+        n_list = np.concatenate((n_list1, n_list2), axis=0)
+
+        print('[INFO] X_test.shape: ' + str(X_test.shape))
+
+        def split_num(name):
+            name = str(name)
+            return len(name.split('.'))  # 长度为2的音频是原始音频
+
+        # 获取原始音频的索引
+        n_list_r = np.reshape(n_list, (-1, 1))
+        n_list_num = np.apply_along_axis(split_num, axis=1, arr=n_list_r)
+        o_indexes = np.where(n_list_num == 2, True, False)  # 原始音频的索引
+
+        n_list = n_list[o_indexes]
+        l_list = l_list[o_indexes]
+        X_test = X_test[o_indexes]
+
+        print('[INFO] 转换数量: %s' % n_list.shape[0])
+
+        X_test = np.transpose(X_test, [0, 2, 1])
+
+        X_test = mx.nd.array(X_test).as_in_context(ctx)
+        print('[INFO] 输入结构: %s' % str(X_test.shape))
+        res = self.model(X_test)
+        print('[INFO] 输出结构: %s' % str(res.shape))
+        data = res.asnumpy()
+        print('[INFO] data.shape: %s' % str(data.shape))
+        oz_arr = np.where(data >= 0.5, 1.0, 0.0).astype(int)  # sigmoid激活函数
+        print oz_arr[0]
+        print np.sum(oz_arr, axis=1)  # 测试分布
+        oz_bin = np.apply_along_axis(self.to_binary, axis=1, arr=oz_arr)
+        print('[INFO] oz_bin: %s' % oz_bin[0])
+
+        out_path = os.path.join(ROOT_DIR, 'experiments', 'data_v2.bin.mx.npz')
+        np.savez(out_path, b_list=oz_bin, l_list=l_list, n_list=n_list)
+
+        print('[INFO] 输出示例: %s %s %s' % (str(oz_bin.shape), bin(oz_bin[0]), oz_bin[0]))
+        print('[INFO] 转换结束')
+
+    @staticmethod
+    def to_binary(bit_list):
+        out = long(0)  # 必须指定为long，否则存储过少
+        # out = 0  # 必须指定为long，否则存储过少
+        for bit in bit_list:
+            out = (out << 1) | bit
+        return out
+
+
+if __name__ == '__main__':
+    hpp = HashPreProcessor()
+    hpp.process()
