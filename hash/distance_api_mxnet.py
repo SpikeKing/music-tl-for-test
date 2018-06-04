65b981456e46bea689d1d716e98d81ad8a4fe510
wcl1 <wcl1@meitu.com>
2018-06-04 11:26:37 +0800

    update

--- hash/distance_api_mxnet.py
+++ hash/distance_api_mxnet.py
@@ -7,6 +7,7 @@ Created by C. L. Wang on 2018/5/8
 
 import os
 import sys
+import collections
 from datetime import datetime
 
 import librosa
@@ -14,28 +15,28 @@ import mxnet as mx
 import numpy as np
 from mxnet import gluon
 
+from data_preprocessor import get_feature
+from root_dir import ROOT_DIR
+from utils import sort_two_list
+
 p = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if p not in sys.path:
     sys.path.append(p)
 
-from training.audio.audio_dir import ROOT_DIR, DATA_DIR
-from training.audio.data_loaders.data_augment import get_feature
-from training.audio.utils.utils import sort_two_list
-from training.audio.utils.config_utils import process_config
-
 
 class DistanceApi(object):
     def __init__(self):
-        self.b_list, self.l_list, self.n_list = self.load_data()
         self.model = None
-        self.config = process_config(os.path.join(ROOT_DIR, 'configs', 'triplet_config.json'))
+        self.data_dir = os.path.join(ROOT_DIR, 'data')
+
+        self.b_list, self.l_list, self.n_list = self.load_data()
+
         pass
 
-    @staticmethod
-    def load_data():
-        file_name = 'data_v2.bin.mx.npz'
+    def load_data(self):
+        file_name = 'data_all.bin.mx.npz'
         print('模型名称: %s' % file_name)
-        data_path = os.path.join(ROOT_DIR, 'experiments', file_name)
+        data_path = os.path.join(self.data_dir, file_name)
         data_all = np.load(data_path)
         b_list = data_all['b_list']
         l_list = data_all['l_list']
@@ -44,7 +45,7 @@ class DistanceApi(object):
 
     def distance(self, audio_id):
         # 获取索引ID
-        i_name = audio_id + '.npy'
+        i_name = audio_id
         i_id = np.where(self.n_list == i_name)
         i_id = int(i_id[0])  # 索引ID
 
@@ -65,13 +66,9 @@ class DistanceApi(object):
 
     def init_mode(self):
         ctx = mx.cpu(0)
-        # self.model = TripletModelMxnet.deep_conv_lstm()
-        # params = os.path.join(ROOT_DIR, "experiments/music_tl_v2/checkpoints", "triplet_loss_model_88_0.9934.params")
-        # print('[INFO] 模型: %s' % params)
-        # self.model.load_params(params, ctx=ctx)
 
-        sym = os.path.join(DATA_DIR, "triplet_net.json")
-        params = os.path.join(DATA_DIR, "triplet_loss_model_88_0.9934.params")
+        sym = os.path.join(self.data_dir, "triplet_net.json")
+        params = os.path.join(self.data_dir, "triplet_loss_model_88_0.9934.params")
         self.model = gluon.nn.SymbolBlock(outputs=mx.sym.load(sym), inputs=mx.sym.var('data'))
         self.model.load_params(params, ctx=ctx)
 
@@ -82,7 +79,6 @@ class DistanceApi(object):
         y_o, sr = librosa.load(mp3_path)
         y, _ = librosa.effects.trim(y_o, top_db=40)  # 去掉空白部分
         features = get_feature(y, sr)
-        # features = np.load('/Users/wang/workspace/music-tl/experiments/npy_data/train/992978262.npy')
 
         features = np.reshape(features, (1, 32, 256))
         features = np.transpose(features, [0, 2, 1])
@@ -109,29 +105,88 @@ class DistanceApi(object):
 
     @staticmethod
     def hamdist(a, b):
-        # 较慢
-        # bits=128
-        # x = (a ^ b) & ((1 << bits) - 1)
-        # ans = 0
-        # while x:
-        #     ans += 1
-        #     x &= x - 1
-        # return ans
         return bin(a ^ b).count('1')  # 更快
 
     @staticmethod
     def to_binary(bit_list):
         out = long(0)  # 必须指定为long，否则存储过少
-        # out = 0  # 必须指定为long，否则存储过少
         for bit in bit_list:
             out = (out << 1) | bit
         return out
 
 
+def duplicates(lst, item):
+    return [i for i, x in enumerate(lst) if x == item]
+
+
+def test_of_acc():
+    da = DistanceApi()  # 接口类
+    label_list = np.unique(da.l_list)  # 标签类别
+    label_indexes = [np.where(da.l_list == i)[0] for i in label_list]  # 标签索引列表
+
+    n_all = 0
+    n_error = 0
+    for idx_list, label in zip(label_indexes, label_list):
+        idx_first = idx_list[0].astype(int)  # 第1个索引值
+        n_idx = len(idx_list)  # 索引数
+        true_list = [da.n_list[idx] for idx in idx_list]  # 真实元素列表
+        print('[INFO] ID: %s' % da.n_list[idx_first])
+        print('[INFO] Num of IDs: %s' % n_idx)
+        try:
+            rb_list, rn_list = da.distance(da.n_list[idx_first])
+        except:
+            continue
+
+        res_indexes = duplicates(rb_list, 0)  # duplicate indexes of same value 0
+        res_list = [rn_list[idx] for idx in res_indexes]  # 最小值的元素
+
+        sub_set = set(true_list) - set(res_list)
+
+        n_all += n_idx
+
+        rb_list, rn_list = sort_two_list(rb_list, rn_list)
+        print '[INFO]',
+        for rb, rn in zip(rb_list, rn_list):
+            print '%s-%s ' % (rb, rn),
+        print('')
+        if sub_set:
+            print(sub_set)
+            n_error += len(sub_set)
+        print('-' * 50)
+    print('总数: %s' % n_all)
+    print('错误: %s' % n_error)
+
+    # label_num_dict = collections.defaultdict(int)
+    # name_label_dict = dict()
+    # for label, name in zip(da.l_list, da.n_list):
+    #     label_num_dict[label] += 1
+    #     name_label_dict[name] = label
+    #
+    # n_error = 0
+    # n_all = 0
+    # for label, name in zip(da.l_list, da.n_list):
+    #     n_same = label_num_dict[label]
+    #     rb_list, rn_list = da.distance(name)
+    #     l_same = rn_list[slice(n_same)]
+    #     for sl in l_same:
+    #         if sl != label:
+    #             n_error += 1  # 写入文档
+    #         else:
+    #             n_all += 1
+    #
+    # print('[INFO] 错误: %s' % n_error)
+    # print('[INFO] 整体: %s' % n_error)
+    # audio_name = '924643775'
+    # print('[INFO] 目标音频: %s' % audio_name)
+    # rb_list, rn_list = da.distance(audio_name)
+    # print('[INFO] 距离: %s' % rb_list)
+    # print('[INFO] 相似: %s' % rn_list)
+
+
 def test_of_distance():
     da = DistanceApi()
     print(da.n_list)
-    audio_name = '993001815'
+    audio_name = '924643775'
     print('[INFO] 目标音频: %s' % audio_name)
     rb_list, rn_list = da.distance(audio_name)
     print('[INFO] 距离: %s' % rb_list)
@@ -150,6 +205,4 @@ def test_of_mp3():
 
 
 if __name__ == '__main__':
-    test_of_distance()
-    print('-' * 50)
-    test_of_mp3()
+    test_of_acc()
